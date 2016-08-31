5bd80df943bdc00549fbe5660f703f3189ebd703
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-08-31 18:32:38 -0500

    factorialize a number

--- app.js
+++ app.js
@@ -2,10 +2,27 @@
 function reverseString(str) {
   var strReverse = [];
   strReverse = str.split('').reverse().join('');
-  str = strReverse;  
+  str = strReverse;
   return str;
 }
 
 console.log(reverseString("hello"));
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+
+// Factorialize a Number
+function factorialize(num) {
+  var totalNum = 2;
+  if ( num === 0) {
+    return num;
+  } else {
+    for (var n=3; n<=num; n++){
+      totalNum *= n;
+    }
+    num = totalNum;
+    return num;
+  }
+}
+
+console.log(factorialize(5));
+console.log(factorialize(0));
